244607c3fd10f36122b8c99f9cecfa89659d36ca
Essa433 <60841988+Essa433@users.noreply.github.com>
2022-06-21 09:08:56 +0300

    Delete create_account.js

--- routs/account/create_account.js
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify"
-
-export function addCreateAccount(server: FastifyInstance) {
-  server.put("/account/create", (request, reply) => {
-    const data = request.body
-    return { name: data.name, phone: data.phone }
-  })
-}
