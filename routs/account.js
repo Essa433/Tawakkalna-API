014a3c4e731ecf283cb9768dfd1cf546f88c8776
Essa433 <60841988+Essa433@users.noreply.github.com>
2022-06-21 09:09:56 +0300

    Delete account.js

--- routs/account.js
+++
@@ -1,8 +0,0 @@
-const { CreateAccount } = require("../controller/account")
-
-const fastifyRoutesAccount = require("fastify")({ logger: true })
-
-// fastify.post("/", CreateAccount)
-fastifyRoutesAccount.put("/account/create", CreateAccount)
-
-module.exports = fastifyRoutesAccount
