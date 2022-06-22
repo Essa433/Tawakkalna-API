aabc18970c727425d10e5bf6a4275017767b3f72
Essa433 <60841988+Essa433@users.noreply.github.com>
2022-06-22 03:26:29 +0300

    Delete src directory

--- src/routes/login.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { request } from "http";
-
-export default async function (server: FastifyInstance ) {
-
-    server.get('login', async (request, reply) => {});
-    
-}
